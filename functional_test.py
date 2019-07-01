8cc5636c45924d41f922660497cf0fd43478fd25
Pavel Kozlov <it.pavelkozlov@gmail.com>
2019-07-01 13:48:10 +0300

    Рефакторизация ФТ

--- functional_test.py
+++ functional_test.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
         '''Демонтаж'''
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
         # Пользователь 1 слышал про крутое новое онлайн-приложение со списком
@@ -42,10 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1 : Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по прежнему приглашает его добавить ещё один элемент.
         # Он вводит "Сделать мушку из павлиньих перьев"
@@ -58,8 +60,8 @@ class NewVisitorTest(unittest.TestCase):
         # Страница снова обновляется, и теперь показывает оба элемента его списка
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Пользователю интересно, запомнит ли сайт его список. Далее он видит, что
         # сайт сгенерировал для него уникальный URL - адрес - об этом
